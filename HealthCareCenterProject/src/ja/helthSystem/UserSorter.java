fe2dab358710fe9945b39c04a82cacb9f45b2c5f
Ami Yamanouchi <amiyamanouchi@Amis-MBP.telus>
2020-11-12 19:07:56 -0800

    first commit

---
+++ HealthCareCenterProject/src/ja/helthSystem/UserSorter.java
@@ -0,0 +1,13 @@
+package ja.helthSystem;
+
+import java.util.Comparator;
+
+public class UserSorter {
+	
+	public static class CompareByName implements Comparator<User> {
+		@Override
+		public int compare(User o1, User o2) {
+			return o1.getLastName().compareTo(o2.getLastName());
+		}
+	}
+}
